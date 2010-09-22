0a04695f05767de921c4dad646c7919d9acf1c48
jetherton <john.etherton@gmail.com>
2010-09-22 16:30:46 +0000

    This makes it so that descriptions aren't required for categories, cause honestly, who cares. That stuff isn't used anywhere else in the website, and if your category needs a desription, then it's not a very good category.

--- application/controllers/admin/manage.php
+++ application/controllers/admin/manage.php
@@ -83,7 +83,7 @@ class Manage_Controller extends Admin_Controller
 				// Add some rules, the input field, followed by a list of checks, carried out in order
 				$post->add_rules('parent_id','required','numeric');
 				$post->add_rules('category_title','required', 'length[3,80]');
-				$post->add_rules('category_description','required');
+				//$post->add_rules('category_description','required'); //this is never used anywhere in the program
 				$post->add_rules('category_color','required', 'length[6,6]');
 				$post->add_rules('category_image', 'upload::valid', 
 					'upload::type[gif,jpg,png]', 'upload::size[50K]');
